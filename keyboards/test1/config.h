d9924cf42c45c3222a1c314c907ee55a2d7a8196
Simon Fischer <simi.fischa@gmail.com>
2024-02-18 21:57:23 +0100

    something working 5x5

---
+++ keyboards/test1/config.h
@@ -0,0 +1,33 @@
+// Copyright 2024 Simon Fischer (@Simon Fischer)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+// #define ENABLE_RGB_MATRIX_PIXEL_RAIN
+// #define RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+//#define RGB_MATRIX_LED_COUNT 1
+//#define WS2812_DI_PIN B5
+
+// #define WS2812_DI_PIN B5
+// #define RGBLED_NUM 2
+
+#define WS2812_DI_PIN B5
+#define RGB_MATRIX_LED_COUNT 25
+
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
---
+++ keyboards/test1/rules.mk
@@ -0,0 +1,6 @@
+# This file intentionally left blank
+RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = ws2812
+
+# RGBLIGHT_ENABLE = yes
+# RGBLIGHT_DRIVER = ws2812
